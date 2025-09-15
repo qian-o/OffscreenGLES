3ee99a006ef0347709b41f014c412f11005e3207
qian-o <1324771795@qq.com>
2025-09-15 22:42:54 +0800

    Add OpenGL rendering setup with shaders and triangle draw
    This commit introduces a basic OpenGL rendering setup in the `Program` class. It includes vertex and fragment shader source code, an `Init` method for compiling and linking shaders, and a modified `Render` method to draw a triangle. The `Init` method is called in `Main` to ensure proper shader initialization before rendering.

--- OffscreenGLES/Program.cs
+++ OffscreenGLES/Program.cs
@@ -20,6 +20,26 @@ internal unsafe class Program
     private readonly static int Height = 1000;
     private readonly static string OutputFilePath = "output.bmp";
 
+    private readonly static string vs = """
+        attribute vec3 position;
+
+        void main()
+        {
+            gl_Position = vec4(position, 1.0);
+        }
+        """;
+
+    private readonly static string fs = """
+        precision mediump float;
+
+        void main()
+        {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+        """;
+
+    private static uint program;
+
     private static void Main(string[] _)
     {
         EglDisplay display = Egl.GetDisplay(0);
@@ -58,17 +78,54 @@ internal unsafe class Program
 
         GL gl = GL.GetApi(new LamdaNativeContext(Egl.GetProcAddress));
 
+        Init(gl);
         Render(gl);
         ReadPixels(gl);
 
         Console.WriteLine($"Output file saved to: {Path.GetFullPath(OutputFilePath)}");
     }
 
+    private static void Init(GL gl)
+    {
+        uint vertexShader = gl.CreateShader(ShaderType.VertexShader);
+        gl.ShaderSource(vertexShader, vs);
+        gl.CompileShader(vertexShader);
+
+        uint fragmentShader = gl.CreateShader(ShaderType.FragmentShader);
+        gl.ShaderSource(fragmentShader, fs);
+        gl.CompileShader(fragmentShader);
+
+        program = gl.CreateProgram();
+        gl.AttachShader(program, vertexShader);
+        gl.AttachShader(program, fragmentShader);
+        gl.LinkProgram(program);
+
+        gl.DeleteShader(vertexShader);
+        gl.DeleteShader(fragmentShader);
+    }
+
     private static void Render(GL gl)
     {
+        float[] vertices =
+        [
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+             0.0f,  0.5f, 0.0f
+        ];
+
         gl.Viewport(0, 0, (uint)Width, (uint)Height);
         gl.ClearColor(0.0f, 0.5f, 0.5f, 1.0f);
         gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
+    
+        gl.UseProgram(program);
+
+        fixed (float* verticesPtr = vertices)
+        {
+            gl.EnableVertexAttribArray(0);
+            gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, (nint)verticesPtr);
+            gl.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            gl.DisableVertexAttribArray(0);
+        }
     }
 
     private static void ReadPixels(GL gl)
